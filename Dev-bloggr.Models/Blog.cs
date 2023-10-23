613062564184601c4786fd19235f8ed257b6aa85
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-10-23 18:24:18 +0100

    added blog model

---
+++ Dev-bloggr.Models/Blog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev_bloggr.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Header { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation property for the user
+        public ApplicationUser User { get; set; }
+        //Foreign key for the user
+        public string UserId { get; set; }
+
+    }
+}
